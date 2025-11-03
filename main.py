e11e5734c13145bf683182c1f8bba3af2be12979
KiMinje <under201@gmail.com>
2025-11-03 22:21:34 +0900

    main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from pico2d import *
+from pico2d import open_canvas, delay, clear_canvas
+import game_framework
 
-open_canvas()
+import play_mode as start_mode
 
+open_canvas(1600, 600)
+game_framework.run(start_mode)
 clear_canvas()
